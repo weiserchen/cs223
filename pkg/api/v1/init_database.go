549269be5eedb44332a18736a9f5efa633998c29
Daniel Wu <wu7115@gmail.com>
2024-12-01 21:17:52 -0800

    Update init_database.go

--- pkg/api/v1/init_database.go
+++ pkg/api/v1/init_database.go
@@ -1,112 +1,312 @@
 package v1
 
 import (
+	"context"
+	"fmt"
 	"math/rand"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	"sync"
 	"time"
 
 	"github.com/brianvoe/gofakeit"
-	"github.com/stretchr/testify/require"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type postgres struct {
+	db *pgxpool.Pool
+}
+
+var (
+	pgInstance *postgres
+	pgOnce     sync.Once
+)
+
+func NewPG(ctx context.Context, connString string) (*postgres, error) {
+	pgOnce.Do(func() {
+		db, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			fmt.Errorf("unable to create connection pool: %w", err)
+			return
+		}
+
+		pgInstance = &postgres{db}
+	})
+
+	return pgInstance, nil
+}
+
+func (pg *postgres) Ping(ctx context.Context) error {
+	return pg.db.Ping(ctx)
+}
+
+func (pg *postgres) Close() {
+	pg.db.Close()
+}
+
+func (pg *postgres) bulkInsertUsers(ctx context.Context, users []User) error {
+	query := `INSERT INTO Users (user_name)
+		VALUES (@userName)`
+	batch := &pgx.Batch{}
+	for _, user := range users {
+		args := pgx.NamedArgs{
+			"userName": user.Name,
+		}
+		batch.Queue(query, args)
+	}
+	results := pg.db.SendBatch(ctx, batch)
+	defer results.Close()
+
+	for i := 0; i < len(users); i++ {
+		_, err := results.Exec()
+		return fmt.Errorf("unable to insert row: %w", err)
+	}
+
+	return results.Close()
+}
+
+func (pg *postgres) bulkInsertEvents(ctx context.Context, events []Event) error {
+	query := `INSERT INTO Events (event_name, event_info, start_at, end_at, location)
+		VALUES (@eventName, @eventInfo, @startAt, @endAt, @loaction)`
+	batch := &pgx.Batch{}
+	for _, event := range events {
+		args := pgx.NamedArgs{
+			"eventName": event.EventName,
+			"eventInfo": event.EventInfo,
+			"startAt":   event.StartAt,
+			"endAt":     event.EndAt,
+			"location":  event.Location,
+		}
+		batch.Queue(query, args)
+	}
+	results := pg.db.SendBatch(ctx, batch)
+	defer results.Close()
+
+	for i := 0; i < len(events); i++ {
+		_, err := results.Exec()
+		return fmt.Errorf("unable to insert row: %w", err)
+	}
+
+	return results.Close()
+}
+
 type User struct {
-	Id         int64
+	ID         int64
 	Name       string
 	HostEvents []int64
 }
 
 type Event struct {
-	Id           int64
-	Name         string
-	HostId       int64
+	EventID      int64
+	EventName    string
+	EventInfo    string
+	StartAt      time.Time
+	EndAt        time.Time
+	Location     string
 	Participants []int64
+	Host         int64
 }
 
-func initUsers(t *testing.T, client *http.Client, serverUser *httptest.Server) []User {
-	gofakeit.Seed(123)
-
-	users := make([]User, 0, 100)
-	userNames := make(map[string]struct{})
-	for len(userNames) < 100 {
-		userName := gofakeit.Name()
-		userNames[userName] = struct{}{}
-	}
-
-	eventIDs := make([]int64, 100)
-	for i := range eventIDs {
-		eventIDs[i] = int64(i + 1)
-	}
-	rand.Shuffle(len(eventIDs), func(i, j int) { eventIDs[i], eventIDs[j] = eventIDs[j], eventIDs[i] })
-	eventIndex := 0
-
-	for userName := range userNames {
-		numEvents := rand.Intn(4)
-		hostEvents := []int64{}
-		for j := 0; j < numEvents && eventIndex < len(eventIDs); j++ {
-			hostEvents = append(hostEvents, eventIDs[eventIndex])
-			eventIndex++
-		}
-
-		requestCreateUser := &RequestCreateUser{
-			UserName:   userName,
-			HostEvents: hostEvents,
-		}
-		responseCreateUser, err := PostRequestCreateUser(client, serverUser.URL, requestCreateUser)
-		require.NoError(t, err, "Failed to create user %s", userName)
-
-		user := User{
-			Id:         responseCreateUser.UserID,
-			Name:       userName,
-			HostEvents: hostEvents,
-		}
-		users = append(users, user)
-		t.Logf("Created user: %+v", user)
-	}
-	return users
+type EventLog struct {
+	LogID     int64
+	EventID   int64
+	UserID    int64
+	EventType string
+	Update    string
+	UpdatedAt time.Time
 }
 
-func initEvents(t *testing.T, client *http.Client, serverEvent *httptest.Server, users []User) []Event {
-	events := make([]Event, 0, 100)
-	for _, user := range users {
-		for _, eventID := range user.HostEvents {
-			numParticipants := rand.Intn(11)
-			participantSet := make(map[int64]struct{})
-			for len(participantSet) < numParticipants {
-				participant := users[rand.Intn(len(users))].Id
-				if participant != user.Id {
-					participantSet[participant] = struct{}{}
-				}
-			}
+func createRandomUsers(initUserNum int) {
+	users := []User{}
+	for i := 0; i < initUserNum; i++ {
+		users = append(users, User{
+			Name: gofakeit.Name(),
+		})
+	}
 
-			participants := make([]int64, 0, len(participantSet))
-			for p := range participantSet {
-				participants = append(participants, p)
-			}
+}
 
-			eventName := gofakeit.Sentence(3)
-			requestCreateEvent := &RequestTxCreateEvent{
-				UserID:       user.Id,
-				EventName:    eventName,
-				EventInfo:    gofakeit.Sentence(10),
-				StartAt:      time.Now(),
-				EndAt:        time.Now().Add(time.Hour),
-				Location:     gofakeit.City(),
-				Participants: participants,
-			}
-			responseCreateEvent, err := PostRequestTxCreateEvent(client, serverEvent.URL, requestCreateEvent)
-			require.NoError(t, err, "Failed to create event %d for user %d", eventID, user.Id)
+func createRandomEvents(initEventNum int) {
+	events := []Event{}
+	for i := 0; i < initEventNum; i++ {
+		events = append(events, Event{
+			EventName: gofakeit.Sentence(3),
+			EventInfo: gofakeit.Sentence(10),
+			StartAt:   gofakeit.Date(),
+			EndAt:     gofakeit.Date().Add(time.Hour),
+			Location:  gofakeit.City(),
+		})
+	}
 
-			event := Event{
-				Id:           responseCreateEvent.EventID,
-				Name:         eventName,
-				HostId:       user.Id,
-				Participants: participants,
-			}
-			events = append(events, event)
-			t.Logf("Created event: %+v", event)
+}
+
+func (pg *postgres) getEventsWithNoHost(ctx context.Context) ([]int64, error) {
+	query := `SELECT ID FROM Events WHERE Host IS NULL`
+	rows, err := pg.db.Query(ctx, query)
+	if err != nil {
+		fmt.Errorf("unable to query events: %w", err)
+	}
+	defer rows.Close()
+	var ids []int64
+	for rows.Next() {
+		var id int64
+		if err := rows.Scan(&id); err != nil {
+			return nil, fmt.Errorf("unable to scan event ID: %w", err)
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
+func (pg *postgres) getRandomUsers(ctx context.Context) ([]int64, error) {
+	query := `SELECT ID FROM Users`
+	rows, err := pg.db.Query(ctx, query)
+	if err != nil {
+		fmt.Errorf("unable to query users: %w", err)
+	}
+	defer rows.Close()
+	var ids []int64
+	for rows.Next() {
+		var id int64
+		if err := rows.Scan(&id); err != nil {
+			return nil, fmt.Errorf("unable to scan user ID: %w", err)
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
+func (pg *postgres) getRandomEvents(ctx context.Context) ([]int64, error) {
+	query := `SELECT ID FROM Events`
+	rows, err := pg.db.Query(ctx, query)
+	if err != nil {
+		fmt.Errorf("unable to query events: %w", err)
+	}
+	defer rows.Close()
+	var ids []int64
+	for rows.Next() {
+		var id int64
+		if err := rows.Scan(&id); err != nil {
+			return nil, fmt.Errorf("unable to scan event ID: %w", err)
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
+func (pg *postgres) makeHost(ctx context.Context) error {
+	eventIDs, err := pg.getEventsWithNoHost(ctx)
+	if err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
+	userIDs, err := pg.getRandomUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
+
+	if len(userIDs) == 0 {
+		return fmt.Errorf("no users available to assign as hosts")
+	}
+
+	for _, eventID := range eventIDs {
+		randomUserID := userIDs[rand.Intn(len(userIDs))]
+
+		query := `UPDATE Events SET host = $1 WHERE ID = $2`
+		_, err := pg.db.Exec(ctx, query, randomUserID, eventID)
+		if err != nil {
+			return fmt.Errorf("failed to update event host: %w", err)
+		}
+
+		query = `SELECT host_events FROM Users WHERE ID = $1`
+		var hostEvents []int64
+		err = pg.db.QueryRow(ctx, query, randomUserID).Scan(&hostEvents)
+		if err != nil {
+			return fmt.Errorf("failed to fetch user's host events: %w", err)
+		}
+		hostEvents = append(hostEvents, eventID)
+
+		query = `UPDATE Users SET host_events = $1 WHERE ID = $2`
+		_, err = pg.db.Exec(ctx, query, hostEvents, randomUserID)
+		if err != nil {
+			return fmt.Errorf("failed to update user's host events: %w", err)
+		}
+
+		query = `SELECT event_info FROM Events WHERE ID = $1`
+		var info string
+		err = pg.db.QueryRow(ctx, query, eventID).Scan(&info)
+		if err != nil {
+			return fmt.Errorf("failed to fetch event info: %w", err)
+		}
+
+		query = `INSERT INTO EventLog (event_id, user_id, event_type, update, update_at) VALUES ($1, $2, $3, $4, $5)`
+		_, err = pg.db.Exec(ctx, query, eventID, randomUserID, "Create", info, time.Now())
+		if err != nil {
+			return fmt.Errorf("failed to insert event log: %w", err)
 		}
 	}
-	return events
+	return nil
+}
+
+func (pg *postgres) makeParticipants(ctx context.Context) error {
+	eventIDs, err := pg.getRandomEvents(ctx)
+	if err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
+	userIDs, err := pg.getRandomUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
+
+	if len(userIDs) == 0 {
+		return fmt.Errorf("no users available to assign as hosts")
+	}
+
+	for _, eventID := range eventIDs {
+		if rand.Intn(2) == 0 {
+			continue
+		}
+
+		var hostID int64
+		query := `SELECT host FROM Events WHERE ID = $1`
+		err := pg.db.QueryRow(ctx, query, eventID).Scan(&hostID)
+		if err != nil {
+			return fmt.Errorf("failed to fetch event host: %w", err)
+		}
+		potentialParticipants := make([]int64, 0)
+		for _, userID := range userIDs {
+			if userID != hostID {
+				potentialParticipants = append(potentialParticipants, userID)
+			}
+		}
+
+		if len(potentialParticipants) == 0 {
+			continue
+		}
+
+		numParticipants := rand.Intn(len(potentialParticipants)) + 1
+		rand.Shuffle(len(potentialParticipants), func(i, j int) {
+			potentialParticipants[i], potentialParticipants[j] = potentialParticipants[j], potentialParticipants[i]
+		})
+		participants := potentialParticipants[:numParticipants]
+
+		query = `UPDATE Events SET participants = $1 WHERE ID = $2`
+		_, err = pg.db.Exec(ctx, query, participants, eventID)
+		if err != nil {
+			return fmt.Errorf("failed to update event participants: %w", err)
+		}
+
+		query = `SELECT event_info FROM Events WHERE ID = $1`
+		var info string
+		err = pg.db.QueryRow(ctx, query, eventID).Scan(&info)
+		if err != nil {
+			return fmt.Errorf("failed to fetch event info: %w", err)
+		}
+		for _, participantID := range participants {
+			query = `INSERT INTO EventLog (event_id, user_id, event_type, update, update_at) VALUES ($1, $2, $3, $4, $5)`
+			_, err = pg.db.Exec(ctx, query, eventID, participantID, "Join", info, time.Now())
+			if err != nil {
+				return fmt.Errorf("failed to insert event log: %w", err)
+			}
+		}
+	}
+	return nil
 }
